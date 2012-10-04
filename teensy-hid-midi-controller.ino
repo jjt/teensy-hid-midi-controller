26e589f6816cc244b8f512ca0eb69f0055e33757
Jason Trill <jason@jasontrill.com>
2012-10-04 16:50:55 -0700

    Minor code cleanup

--- teensy-hid-midi-controller.ino
+++ teensy-hid-midi-controller.ino
@@ -2,22 +2,15 @@
 
 const int channel = 1;
 const int vel = 99;
-const int num_btns = 23; // One more than we need because pin 6 is nixed
+const int num_btns = 23; 
 const int root_note = 48; // C3
 const int bounce_rate = 5;
 
-// Controller pin layout as of Oct 5th, 2012
-//  
-//  3   6   9   4  13  17  20
-//  2   7   10  5  14  18  21 
-//  1   8   11  0  15  19  22
-//          12     16
-//
 
 
 // This will be the note order from root_note to root_note + 22
 // Could make a vector of pointers, but things went weird on me 
-// when I tried that, so here we are
+// when it came to loop(), so here we are
 Bounce buttons[] = {
   Bounce(0,bounce_rate),
   Bounce(1,bounce_rate),
@@ -44,6 +37,13 @@ Bounce buttons[] = {
   Bounce(23,bounce_rate)
 };
 
+// Controller pin layout as of Oct 5th, 2012
+//  
+//  3   6   9   4  13  17  20
+//  2   7   10  5  14  18  21 
+//  1   8   11  0  15  19  22
+//          12     16
+//
 // Pinouts don't match the notes we want so translate them
 // Based on array index, not the pin numbers (jumps at 6)
 int button_trans[num_btns] = {5,2,9,16,19,12,17,10,3,18,11,4,0,20,13,6,1,21,14,7,22,15,8};
